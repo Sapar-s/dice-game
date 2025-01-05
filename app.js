07b2f81a0b5b7368eb497d5e3ee8ffc8e779be64
Sapar Smagul <smagulsapar52@gmail.com>
2025-01-05 20:56:00 +0800

    asd

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // Togloomiin buh gazart ashiglah huvsagch zarlay
 var activePlayer, scores, roundScore;
 
+// Game duussan esehiig hadgalah toloviin huvsagch
+var isNewGame;
+
 // Shoonii zurgiig uzuuleh elementiig haij olood end hadgalah
 var diceDom = document.querySelector(".dice");
 
 // Shine togloom ehluuleh function
 function initGame() {
+  // Togloom ehellee gedeg tolovt oruulna.
+  isNewGame = true;
+
   activePlayer = 0;
 
   scores = [0, 0];
@@ -17,51 +23,84 @@ function initGame() {
   document.getElementById("current-0").textContent = 0;
   document.getElementById("current-1").textContent = 0;
 
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+
   diceDom.style.display = "none";
 }
 
 initGame();
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
-
-  diceDom.style.display = "block";
-  diceDom.src = "dice-" + diceNumber + ".png";
-
-  // buusan too ni 1 ees ylgaatai bol idevhtei toglogchiin eeljiin onoog nemegduulen.
-  if (diceNumber !== 1) {
-    // 1 ees oor too buunaa , buusan toog toglogchid nemj ogno
-    roundScore += diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  if (!isNewGame) {
+    alert("Тоглоом дууссан байна. NEW GAME товчийг дарж эхлэнэ үү");
   } else {
-    // Daraagiin toglogchid shiljuulen
-    switchToNextPlayer();
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+
+    // buusan too ni 1 ees ylgaatai bol idevhtei toglogchiin eeljiin onoog nemegduulen.
+    if (diceNumber !== 1) {
+      // 1 ees oor too buunaa , buusan toog toglogchid nemj ogno
+      roundScore += diceNumber;
+      document.getElementById("current-" + activePlayer).textContent =
+        roundScore;
+    } else {
+      // Daraagiin toglogchid shiljuulen
+      switchToNextPlayer();
+    }
   }
+
+  //  else {
+  // alert("Тоглоом дууссан байна. NEW GAME товчийг дарж эхлэнэ үү");
+  // }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  // Ug toglogchiin tsugluulsan eeljiin onoog global onoon deer n nemj ogno
-  scores[activePlayer] += roundScore;
-
-  // Delgets deer onoog ni oorchlono
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
-
-  // Ug toglogch hojson esehiig shalgah function
-  if (scores[activePlayer] >= 100) {
-    // Ylagch gesen text gargana
-    document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
+  if (!isNewGame) {
+    alert("Тоглоом дууссан байна. NEW GAME товчийг дарж эхлэнэ үү");
   } else {
-    // Toglogchiin eeljiig solino
-    switchToNextPlayer();
+    // Ug toglogchiin tsugluulsan eeljiin onoog global onoon deer n nemj ogno
+    scores[activePlayer] += roundScore;
+
+    // Delgets deer onoog ni oorchlono
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
+
+    // Ug toglogch hojson esehiig shalgah function
+    if (scores[activePlayer] >= 100) {
+      // Game duussan tolovt oruulna
+      isNewGame = false;
+
+      // Ylagch gesen text gargana
+      document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+
+      diceDom.style.display = "none";
+    } else {
+      // Toglogchiin eeljiig solino
+      switchToNextPlayer();
+    }
   }
+
+  //  else {
+  //     alert("Тоглоом дууссан байна. NEW GAME товчийг дарж эхлэнэ үү");
+  //   }
 });
 
 function switchToNextPlayer() {
